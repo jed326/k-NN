7511f215ea2fbd02e68f2568ef17526553eb6e1d
Jay Deng <jayd0104@gmail.com>
2025-02-21 13:11:39 -0800

    Add vector data upload implementation to RemoteIndexBuildStrategy
    Signed-off-by: Jay Deng <jayd0104@gmail.com>

--- src/main/java/org/opensearch/knn/index/codec/nativeindex/NativeIndexBuildStrategyFactory.java
+++ src/main/java/org/opensearch/knn/index/codec/nativeindex/NativeIndexBuildStrategyFactory.java
@@ -7,14 +7,18 @@ package org.opensearch.knn.index.codec.nativeindex;
 
 import org.apache.lucene.index.FieldInfo;
 import org.opensearch.index.IndexSettings;
+import org.opensearch.knn.common.featureflags.KNNFeatureFlags;
 import org.opensearch.knn.index.codec.nativeindex.remote.RemoteIndexBuildStrategy;
 import org.opensearch.knn.index.engine.KNNEngine;
+import org.opensearch.knn.index.vectorvalues.KNNVectorValues;
 import org.opensearch.repositories.RepositoriesService;
 
+import java.io.IOException;
 import java.util.function.Supplier;
 
 import static org.opensearch.knn.common.FieldInfoExtractor.extractKNNEngine;
 import static org.opensearch.knn.common.KNNConstants.MODEL_ID;
+import static org.opensearch.knn.index.codec.util.KNNCodecUtil.initializeVectorValues;
 
 /**
  * Creates the {@link NativeIndexBuildStrategy}
@@ -38,7 +42,11 @@ public final class NativeIndexBuildStrategyFactory {
      * @param fieldInfo
      * @return
      */
-    public NativeIndexBuildStrategy getBuildStrategy(final FieldInfo fieldInfo) {
+    public NativeIndexBuildStrategy getBuildStrategy(
+        final FieldInfo fieldInfo,
+        final int totalLiveDocs,
+        final KNNVectorValues<?> knnVectorValues
+    ) throws IOException {
         final KNNEngine knnEngine = extractKNNEngine(fieldInfo);
         boolean isTemplate = fieldInfo.attributes().containsKey(MODEL_ID);
         boolean iterative = !isTemplate && KNNEngine.FAISS == knnEngine;
@@ -47,11 +55,15 @@ public final class NativeIndexBuildStrategyFactory {
             ? MemOptimizedNativeIndexBuildStrategy.getInstance()
             : DefaultIndexBuildStrategy.getInstance();
 
-        if (repositoriesServiceSupplier != null
+        initializeVectorValues(knnVectorValues);
+        long vectorBlobLength = ((long) knnVectorValues.bytesPerVector()) * totalLiveDocs;
+
+        if (KNNFeatureFlags.isKNNRemoteVectorBuildEnabled()
+            && repositoriesServiceSupplier != null
             && indexSettings != null
             && knnEngine.supportsRemoteIndexBuild()
-            && RemoteIndexBuildStrategy.shouldBuildIndexRemotely(indexSettings)) {
-            return new RemoteIndexBuildStrategy(repositoriesServiceSupplier, strategy);
+            && RemoteIndexBuildStrategy.shouldBuildIndexRemotely(indexSettings, vectorBlobLength)) {
+            return new RemoteIndexBuildStrategy(repositoriesServiceSupplier, strategy, indexSettings);
         } else {
             return strategy;
         }
